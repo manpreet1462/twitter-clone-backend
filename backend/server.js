cff960a7e1fe9fd7ccf9be610b270a7b4086350a
manpreet1462 <80613332+manpreet1462@users.noreply.github.com>
2024-11-08 01:48:42 +0530

    1-post routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import {v2 as cloudinary} from "cloudinary"
 import connectMongoDB from "./db/DB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js"
+import postRoutes from './routes/postRoutes.js'
 dotenv.config();
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
@@ -17,8 +18,12 @@ const PORT = process.env.PORT || 5000
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+//auth hai bhai
 app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes)
+//user hai bhai
+app.use("/api/users",userRoutes);
+//posts hai bhai
+app.use("/api/posts",postRoutes);
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
