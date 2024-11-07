2786368c298a80cdd58964c0fe19cee03766b887
manpreet1462 <80613332+manpreet1462@users.noreply.github.com>
2024-11-08 02:13:02 +0530

    1- notifications routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectMongoDB from "./db/DB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js"
 import postRoutes from './routes/postRoutes.js'
+import notificationRoutes from "./routes/notificationRoutes.js"
 dotenv.config();
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
@@ -18,12 +19,15 @@ const PORT = process.env.PORT || 5000
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+//notification
+app.use("/api/notifications",notificationRoutes);
 //auth hai bhai
 app.use("/api/auth",authRoutes);
 //user hai bhai
 app.use("/api/users",userRoutes);
 //posts hai bhai
 app.use("/api/posts",postRoutes);
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
